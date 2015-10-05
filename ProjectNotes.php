b9755f138425c5f52347701274ef15563d9c35ef
tylerthomas <tthomas106@mail.valenciacollege.edu>
2015-10-05 19:42:01 +0000

    fdds

---
+++ ProjectNotes.php
@@ -0,0 +1,12 @@
+<?php
+
+    // Create a list of words that are associated with each othere for different commonly searched words.
+    // Link those words together
+    // Organize the potential results for each term based on what would be the most likely searched for (Incorporate recent searches somehow/commonly searched items?)
+    // Get user input for the term they're searching
+    // Display the results
+
+
+
+
+?>
